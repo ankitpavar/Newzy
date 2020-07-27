50a225751e7306adc73ebc1795d1f9ace6795cb9
Ankit Pavar <geekyanky@gmail.com>
2020-07-27 14:06:32 +0530

    addes share now label in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -51,7 +51,7 @@ const Card = ({ newzy }) => {
             </div>
           </div>
         </div>
-        <div className="px-3 py-0 mt-2 flex justify-between items-center">
+        <div className="px-3 py-0 mt-0 flex justify-between items-center">
           <a
             href={url}
             target="_blank"
@@ -62,23 +62,23 @@ const Card = ({ newzy }) => {
           </a>
 
           <div className="text-gray-400 mr-3 inline-flex items-center justify-around lg:ml-auto md:ml-auto ml-auto leading-none text-sm pr-3 py-1 ">
-            {/* <div className="border border-red-500 rounded-full mr-1"> */}
-              {/* <svg className='bg-transparent h-5 w-5' fill="currentColor" viewBox="0 0 20 20">
-                <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"></path>
-              </svg> */}
-            {/* </div> */}
-            <WhatsappShareButton url={url}>
-              <WhatsappIcon size={22} round={true} />
-            </WhatsappShareButton>
-            <FacebookShareButton url={url}>
-              <FacebookIcon size={22} round={true} />
-            </FacebookShareButton>
-            <TwitterShareButton url={url}>
-              <TwitterIcon size={22} round={true} />
-            </TwitterShareButton>
-            <TelegramShareButton url={url}>
-              <TelegramIcon size={22} round={true} />
-            </TelegramShareButton>
+            <div className='flex flex-col'>
+              <div className="text-gray-400 text-xs italic mb-1">Share now</div>
+              <div >
+                <WhatsappShareButton url={url}>
+                  <WhatsappIcon size={22} round={true} />
+                </WhatsappShareButton>
+                <FacebookShareButton url={url}>
+                  <FacebookIcon size={22} round={true} />
+                </FacebookShareButton>
+                <TwitterShareButton url={url}>
+                  <TwitterIcon size={22} round={true} />
+                </TwitterShareButton>
+                <TelegramShareButton url={url}>
+                  <TelegramIcon size={22} round={true} />
+                </TelegramShareButton>
+              </div>
+            </div>
           </div>
         </div>
       </div>
