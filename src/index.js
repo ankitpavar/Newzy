33236c6d77a5ab256e1c560587ebac03c002a123
Ankit Pavar <ankit.pavar@spacecode.com>
2021-11-30 22:05:48 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './assets/main.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./assets/main.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.render(
-    <App />
-  ,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
+serviceWorkerRegistration.register();
